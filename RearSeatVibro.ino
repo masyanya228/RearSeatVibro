ab2308c98c288a44a9c21be651482092c0c7e6f1
marsel-tecno <marsel.khabibullin.99@gmail.com>
2026-09-19 08:40:05 +0300

    [ref]: mid ref

--- RearSeatVibro.ino
+++ RearSeatVibro.ino
@@ -259,38 +259,22 @@ void loop() {
 }
 
 //0-left; 1-right
-void ClickHardware(int seatNum){
+void ClickHardware(uint8_t seatNum){
   if(seatNum==0)
-  {
-    L_Mode=GetNextMode(L_Mode);
-    logB("Seat #0", L_Mode);
-    seatL.mode=L_Mode;
-  }
+    seatL.mode=GetNextMode(seatL.mode);
   else if(seatNum==1)
-  {
-    R_Mode=GetNextMode(R_Mode);
-    logB("Seat #1", R_Mode);
-    seatR.mode=R_Mode;
-  }
+    seatR.mode=GetNextMode(seatR.mode);
 }
 
 void SetHardware(uint8_t seatNum, uint8_t mode){
   if(seatNum==0)
-  {
-    L_Mode=mode;
-    logB("Seat #0", L_Mode);
-    seatL.mode=L_Mode;
-  }
+    seatL.mode=mode;
   else if(seatNum==1)
-  {
-    R_Mode=mode;
-    logB("Seat #1", R_Mode);
-    seatR.mode=R_Mode;
-  }
+    seatR.mode=mode;
 }
 
 //Возвращает номер следующего режима.
-byte GetNextMode(byte mode){
+uint8_t GetNextMode(uint8_t mode){
   int i=0;
   while(i<4){
     if(modeSeq[i]==mode){
@@ -303,92 +287,60 @@ byte GetNextMode(byte mode){
   return modeSeq[i];
 }
 
-byte GetIndicator(byte seatNum){
+uint8_t GetIndicator(uint8_t seatNum){
   if(seatNum==0)
-  {
-    logB("Seat #0", L_Mode);
     return L_Mode;
-  }
   else if(seatNum==1)
-  {
-    logB("Seat #1", R_Mode);
     return R_Mode;
-  }
-  else{
+  else
     return 0;
-  }
 }
 
 //I2C commands
-void cmdMode(const uint8_t* buf, uint8_t len) {
-  Serial.print("cmdMode ");
+void cmdMode(const uint8_t* buf, uint8_t len){
   if (len < 1) { slave.respondByte(0x00); return; }
   uint8_t seat = 2;
   if(buf[0]==REG_L_MODE)
     seat=0;
-  if(buf[0]==REG_R_MODE)
+  else if(buf[0]==REG_R_MODE)
     seat=1;
-  Serial.println(seat);
-  if (seat > 1) { slave.respondByte(0x00); return; }
+  else{ slave.respondByte(0x00); return; }
   ClickHardware(seat);
-  uint8_t ind=GetIndicator(seat);
+  uint8_t ind = GetIndicator(seat);
   uint8_t resp[2] = {1, ind};
+  Serial.printf("Mode seat: %02x | fact: %02X\n", seat, ind);
   slave.respond(resp, sizeof(resp));
 }
 
-void cmdSetMode(const uint8_t* buf, uint8_t len) {
-  Serial.print("cmdSetMode ");
+void cmdSetMode(const uint8_t* buf, uint8_t len){
   if (len < 2) { slave.respondByte(0x00); return; }
   uint8_t seat = 2;
   if(buf[0]==REG_L_SET_MODE)
     seat=0;
-  if(buf[0]==REG_R_SET_MODE)
+  else if(buf[0]==REG_R_SET_MODE)
     seat=1;
-  Serial.println(seat);
-  if (seat > 1) { slave.respondByte(0x00); return; }
+  else{ slave.respondByte(0x00); return; }
   SetHardware(seat, buf[1]);
-  uint8_t ind=GetIndicator(seat);
+  uint8_t ind = GetIndicator(seat);
   uint8_t resp[2] = {1, ind};
+  Serial.printf("SetMode seat: %02x | target: %02x | fact: %02X\n", seat, buf[1], ind);
   slave.respond(resp, sizeof(resp));
 }
 
-void cmdGetStatus(const uint8_t* buf, uint8_t len) {
-  Serial.print("cmdGetStatus ");
+void cmdGetStatus(const uint8_t* buf, uint8_t len){
   if (len < 1) { slave.respondByte(0x00); return; }
   uint8_t seat = 2;
   if(buf[0]==REG_L_GetStatus)
     seat=0;
-  if(buf[0]==REG_R_GetStatus)
+  else if(buf[0]==REG_R_GetStatus)
     seat=1;
-  Serial.println(seat);
-  if (seat > 1) { slave.respondByte(0x00); return; }
+  else{ slave.respondByte(0x00); return; }
   uint8_t ind=GetIndicator(seat);
   uint8_t resp[2] = {1, ind};
+  Serial.printf("GetStatus seat: %02x | fact: %02X\n", seat, ind);
   slave.respond(resp, sizeof(resp));
 }
 
-void cmdPing(const uint8_t*, uint8_t) {
+void cmdPing(const uint8_t*, uint8_t){
   slave.respondByte(0x01);
-}
-
-void logS(String str){
-  if(!isDebug)
-    return;
-  Serial.println(str);
-}
-
-void logI(String str, int i){
-  if(!isDebug)
-    return;
-  Serial.print(str);
-  Serial.print(" : ");
-  Serial.println(i);
-}
-
-void logB(String str, byte i){
-  if(!isDebug)
-    return;
-  Serial.print(str);
-  Serial.print(" : ");
-  Serial.println(i);
 }
